ff3b8fd0e80811c5cf250c23156bdc72880e4d14
Jake Dudum <jakedudum@gmail.com>
2020-03-09 14:37:56 -0700

    Added name to navbar and styled navbar to be semi transparent. Also imported all components into App.js and loaded Navbar component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import Contact from './components/Contact';
+import Projects from './components/Projects';
+import AboutMe from './components/AboutMe';
 import './App.css';
 
-function App() {
-  return (
-   <div></div>
-  );
+class App extends React.Component {
+  state = {
+    currentPage: "AboutMe"
+  };
+
+  render() {
+    return (
+      <Navbar></Navbar>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/navbar/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "./style.css";
+
+class Navbar extends Component {
+    render() {
+        return (
+            <nav className="navbar">
+                <h1>Jake Dudum</h1>
+            </nav>
+        )
+    }
+}
+
+export default Navbar;
