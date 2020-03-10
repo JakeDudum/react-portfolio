e9d91c0e0ee19f01111afac1e278b5a20ef0e568
Jake Dudum <jakedudum@gmail.com>
2020-03-10 16:54:20 -0700

    Added images folder with some project pictures to src and put in info and links for first project to test styling.

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -3,7 +3,18 @@ import "./style.css";
 
 function Projects() {
     return (
-        <p>Even more testing</p>
+        <div>
+            <img src="..." alt="RPS-multiplayer Site" />
+            <div>
+                <h5>RPS-Multiplayer</h5>
+                <p>An online multiplayer version of Rock-Paper-Scissors. Only two
+                players can play at any given time and only those two players can use the chat
+                functionality of the site. Player 1 always goes first followed by Player 2.
+                </p>
+                <a href="https://github.com/JakeDudum/RPS-Multiplayer" target="_blank">Github Repo</a>
+                <a href="https://jakedudum.github.io/RPS-Multiplayer/" target="_blank">Deployed Site</a>
+            </div>
+        </div>
     )
 }
 
