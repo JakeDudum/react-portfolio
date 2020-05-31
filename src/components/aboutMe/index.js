c8d66869e39c780baa22a5e58fff9ac677136600
Jake Dudum <jakedudum@gmail.com>
2020-05-31 00:03:15 -0700

    Updated bio section in aboutMe and improved mobile responsiveness across all screen sizes for better viewing experience. Also added a left margin to name in navbar and gave it better sizing for small screens.

--- src/components/aboutMe/index.js
+++ src/components/aboutMe/index.js
@@ -26,14 +26,18 @@ function AboutMe() {
                     <h3>HELLO WORLD!</h3>
                 </div>
                 <div id="bio">
-                    <p>Hi my name if you didn't already know is Jake Dudum. I am a full stack web developer currently working at Trilogy Education Services as a teaching
+                    <p className="tab">Hi my name if you didn't already know is Jake Dudum. I am a full stack web developer currently working at Trilogy Education Services as a teaching
                     assistant helping teach students from all walks of life how to code in MERN stack. Learning how to code is one thing, but teaching others to do it gives 
                     one a whole new perspective on things and really hammers home the importance of the basics and fundamentals of coding. You also get to see a whole lot of 
                     error screens and comile errors you may never have seen before accompanied of course with plenty of red text which you get to solve which is what I love doing, 
-                    solving problems. Admittedly it took me well over seven years to solve the problem of what I really wanted to do in life. I worked in the customer service, 
+                    solving problems.</p>
+                    <br></br>
+                    <p className="tab">
+                    Admittedly it took me well over seven years to solve the problem of what I really wanted to do in life. I worked in the customer service, 
                     food service, and delivery industries during those years and though I never hated my work I also never saw myself doing it for the rest of my life. Thankfully I 
-                    figured out that I love coding in no small part thanks to my brother, Jerry, and my friend Darryl Tolentino. 
-                        </p>
+                    figured out that I love coding in no small part thanks to my brother, Jerry, and my friend Darryl. On a much lighter note I enjoy playing video games in
+                    my free time on my custom built gaming PC and am an achievement hunter/completionist.
+                    </p>
                     <div id="links">
                         <div className="linkContainer">
                             <a href="https://www.linkedin.com/in/jake-dudum-45a967185/" target="_blank" rel="noopener noreferrer">
