9e6e91e3961c98d3992914975d369e9221471cc4
Jake Dudum <jakedudum@gmail.com>
2021-04-23 23:59:57 -0700

    Added scroll animations to projects and modified isScrolledIntoView function in root index.js to accept an offset in order to animate a bit before the entire element is in view.

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -10,97 +10,99 @@ function Projects() {
         <div id="project-background">
             <div className="center">
                 <h1 id="project-title">Projects</h1>
-                <div className="project">
-                    <div className="image-container">
-                        <div className="tech-used">
-                            <p className="tech">HTML</p>
-                            <p className="tech">CSS</p>
-                            <p className="tech">JavaScript</p>
-                            <p className="tech">React</p>
-                            <p className="tech">MySQL</p>
-                            <p className="tech">Node</p>
-                            <p className="tech">Express</p>
-                            <p className="tech">Passport</p>
+                <div className="scroll-animations">
+                    <div className="project animateL">
+                        <div className="image-container">
+                            <div className="tech-used">
+                                <p className="tech">HTML</p>
+                                <p className="tech">CSS</p>
+                                <p className="tech">JavaScript</p>
+                                <p className="tech">React</p>
+                                <p className="tech">MySQL</p>
+                                <p className="tech">Node</p>
+                                <p className="tech">Express</p>
+                                <p className="tech">Passport</p>
+                            </div>
+                            <img className="image" src={Jumble} alt="Jumble Site" />
                         </div>
-                        <img className="image" src={Jumble} alt="Jumble Site" />
-                    </div>
-                    <div className="project-content">
-                        <h5>Jumble</h5>
-                        <p className="description">An app for project management that is user-data driven.
-                        Users can sign up for an account and add projects by filling in a new project
-                        form that will ask for a project name, due date, budget for various departments, and
+                        <div className="project-content">
+                            <h5>Jumble</h5>
+                            <p className="description">An app for project management that is user-data driven.
+                            Users can sign up for an account and add projects by filling in a new project
+                            form that will ask for a project name, due date, budget for various departments, and
                         an initial task with due date.</p>
-                        <a className="projectLink" href="https://github.com/JakeDudum/jumble-dash" target="_blank" rel="noopener noreferrer">Github Repo</a>
-                        <a className="projectLink right" href="https://jumble-dash.herokuapp.com/" target="_blank" rel="noopener noreferrer">Deployed Site</a>
-                    </div>
-                </div>
-
-                <div className="project">
-                    <div className="image-container">
-                        <div className="tech-used">
-                            <p className="tech">HTML</p>
-                            <p className="tech">CSS</p>
-                            <p className="tech">JavaScript</p>
-                            <p className="tech">Bootstrap</p>
-                            <p className="tech">MySQL</p>
-                            <p className="tech">Node</p>
-                            <p className="tech">Express</p>
-                            <p className="tech">Passport</p>
+                            <a className="projectLink" href="https://github.com/JakeDudum/jumble-dash" target="_blank" rel="noopener noreferrer">Github Repo</a>
+                            <a className="projectLink right" href="https://jumble-dash.herokuapp.com/" target="_blank" rel="noopener noreferrer">Deployed Site</a>
                         </div>
-                        <img className="image" src={Bark} alt="Bark Site" />
                     </div>
-                    <div className="project-content">
-                        <h5>Bark</h5>
-                        <p className="description">An app inspired by Yik-Yak. Users can sign up for account and voice
-                        whatever they want to those in their city. The site features a basic user profile to
+
+                    <div className="project animateR">
+                        <div className="image-container">
+                            <div className="tech-used">
+                                <p className="tech">HTML</p>
+                                <p className="tech">CSS</p>
+                                <p className="tech">JavaScript</p>
+                                <p className="tech">Bootstrap</p>
+                                <p className="tech">MySQL</p>
+                                <p className="tech">Node</p>
+                                <p className="tech">Express</p>
+                                <p className="tech">Passport</p>
+                            </div>
+                            <img className="image" src={Bark} alt="Bark Site" />
+                        </div>
+                        <div className="project-content">
+                            <h5>Bark</h5>
+                            <p className="description">An app inspired by Yik-Yak. Users can sign up for account and voice
+                            whatever they want to those in their city. The site features a basic user profile to
                         view old posts and a few simple categories along with a like counter.</p>
-                        <a className="projectLink" href="https://github.com/JakeDudum/Bark" target="_blank" rel="noopener noreferrer">Github Repo</a>
-                        <a className="projectLink right" href="https://tranquil-bastion-82369.herokuapp.com/" target="_blank" rel="noopener noreferrer">Deployed Site</a>
-                    </div>
-                </div>
-
-                <div className="project">
-                    <div className="image-container">
-                        <div className="tech-used">
-                            <p className="tech">HTML</p>
-                            <p className="tech">CSS</p>
-                            <p className="tech">JavaScript</p>
-                            <p className="tech">jQuery</p>
-                            <p className="tech">Materialize</p>
-                            <p className="tech">Firebase</p>
+                            <a className="projectLink" href="https://github.com/JakeDudum/Bark" target="_blank" rel="noopener noreferrer">Github Repo</a>
+                            <a className="projectLink right" href="https://tranquil-bastion-82369.herokuapp.com/" target="_blank" rel="noopener noreferrer">Deployed Site</a>
                         </div>
-                        <img className="image" src={Where2P} alt="Where2P Site" />
                     </div>
-                    <div className="project-content">
-                        <h5>Where2P</h5>
-                        <p className="description">An app designed to help people find a bathroom quick.
-                        The app uses multiple API's including Google Maps API, Yelp API, and
-                        Google Places API in order to help the user find the closest
+
+                    <div className="project animateL">
+                        <div className="image-container">
+                            <div className="tech-used">
+                                <p className="tech">HTML</p>
+                                <p className="tech">CSS</p>
+                                <p className="tech">JavaScript</p>
+                                <p className="tech">jQuery</p>
+                                <p className="tech">Materialize</p>
+                                <p className="tech">Firebase</p>
+                            </div>
+                            <img className="image" src={Where2P} alt="Where2P Site" />
+                        </div>
+                        <div className="project-content">
+                            <h5>Where2P</h5>
+                            <p className="description">An app designed to help people find a bathroom quick.
+                            The app uses multiple API's including Google Maps API, Yelp API, and
+                            Google Places API in order to help the user find the closest
                         bathroom available and features user reviews as well.</p>
-                        <a className="projectLink" href="https://github.com/JakeDudum/Where2p" target="_blank" rel="noopener noreferrer">Github Repo</a>
-                        <a className="projectLink right" href="https://jakedudum.github.io/Where2p/" target="_blank" rel="noopener noreferrer">Deployed Site</a>
-                    </div>
-                </div>
-
-                <div className="project">
-                    <div className="image-container">
-                        <div className="tech-used">
-                            <p className="tech">HTML</p>
-                            <p className="tech">CSS</p>
-                            <p className="tech">JavaScript</p>
-                            <p className="tech">jQuery</p>
-                            <p className="tech">Bootstrap</p>
-                            <p className="tech">Firebase</p>
+                            <a className="projectLink" href="https://github.com/JakeDudum/Where2p" target="_blank" rel="noopener noreferrer">Github Repo</a>
+                            <a className="projectLink right" href="https://jakedudum.github.io/Where2p/" target="_blank" rel="noopener noreferrer">Deployed Site</a>
                         </div>
-                        <img className="image" src={RPS} alt="RPS-multiplayer Site" />
                     </div>
-                    <div className="project-content">
-                        <h5>RPS-Multiplayer</h5>
-                        <p className="description">An online multiplayer version of Rock-Paper-Scissors. Only two
-                        players can play at any given time and only those two players can use the chat
+
+                    <div className="project animateR">
+                        <div className="image-container">
+                            <div className="tech-used">
+                                <p className="tech">HTML</p>
+                                <p className="tech">CSS</p>
+                                <p className="tech">JavaScript</p>
+                                <p className="tech">jQuery</p>
+                                <p className="tech">Bootstrap</p>
+                                <p className="tech">Firebase</p>
+                            </div>
+                            <img className="image" src={RPS} alt="RPS-multiplayer Site" />
+                        </div>
+                        <div className="project-content">
+                            <h5>RPS-Multiplayer</h5>
+                            <p className="description">An online multiplayer version of Rock-Paper-Scissors. Only two
+                            players can play at any given time and only those two players can use the chat
                         functionality of the site. Player 1 always goes first followed by Player 2.</p>
-                        <a className="projectLink" href="https://github.com/JakeDudum/RPS-Multiplayer" target="_blank" rel="noopener noreferrer">Github Repo</a>
-                        <a className="projectLink right" href="https://jakedudum.github.io/RPS-Multiplayer/" target="_blank" rel="noopener noreferrer">Deployed Site</a>
+                            <a className="projectLink" href="https://github.com/JakeDudum/RPS-Multiplayer" target="_blank" rel="noopener noreferrer">Github Repo</a>
+                            <a className="projectLink right" href="https://jakedudum.github.io/RPS-Multiplayer/" target="_blank" rel="noopener noreferrer">Deployed Site</a>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/index.js
+++ src/index.js
@@ -14,21 +14,39 @@ serviceWorker.unregister();
 
 // Scroll function courtesy of Scott Dowding; http://stackoverflow.com/questions/487073/check-if-element-is-visible-after-scrolling
 $(document).ready(function () {
-    function isScrolledIntoView(elem) {
+    function isScrolledIntoView(elem, offset) {
         var docViewTop = $(window).scrollTop();
         var docViewBottom = docViewTop + $(window).height();
 
         var elemTop = $(elem).offset().top;
         var elemBottom = elemTop + $(elem).height();
 
-        return elemBottom <= docViewBottom && elemTop >= docViewTop;
+        return elemBottom <= docViewBottom+offset && elemTop >= docViewTop-offset;
     }
     // If element is scrolled into view, fade it in
     $(window).scroll(function () {
         $(".scroll-animations .animated").each(function () {
-            if (isScrolledIntoView(this) === true) {
+            if (isScrolledIntoView(this, 0) === true) {
                 $(this).addClass("fadeInLeft");
                 $(".fadeInLeft").css("opacity", 1);
+            }
+        });
+    });
+
+    $(window).scroll(function () {
+        $(".scroll-animations .animateL").each(function () {
+            if (isScrolledIntoView(this, 50) === true) {
+                $(this).addClass("animateLeft");
+                $(".animateLeft").css("opacity", 1);
+            }
+        });
+    });
+
+    $(window).scroll(function () {
+        $(".scroll-animations .animateR").each(function () {
+            if (isScrolledIntoView(this, 50) === true) {
+                $(this).addClass("animateRight");
+                $(".animateRight").css("opacity", 1);
             }
         });
     });
