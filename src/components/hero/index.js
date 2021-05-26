52470a8f5075eb8b2f8ac6019906144273cc8ceb
Jake Dudum <jakedudum@gmail.com>
2021-05-25 22:21:30 -0700

    Moved links back to hero(formerly aboutMe) component and added some basic headings. Redid typewriter animation. Edited navbar component styling and removed name from it. Adjusted global font-family and edited font-size as a result throughout components. Also changed projects background image.

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithubSquare } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import "./style.css";
 
 function Hero() {
     return (
         <section id="landing">
-            <div className="typewriter">
-                <h3>HELLO  WORLD!</h3>
+            <p id="typewriter">Hello world!</p>
+            <div id="about">
+                <div id="heading">My name is Jake Dudum.</div>
+                <div id="sub-heading">I'm a Full Stack Web Developer located in San Bruno, California.</div>
+                <div id="sub-heading">Check out my work below or shoot me over an email to get in touch.</div>
+                <div id="links">
+                    <div className="linkContainer">
+                        <a href="https://www.linkedin.com/in/jakedudum" class="iconLink" target="_blank" rel="noopener noreferrer">
+                            <FontAwesomeIcon id="linkedin" icon={faLinkedin} size="3x" />
+                        </a>
+                    </div>
+                    <div className="linkContainer">
+                        <a href="https://github.com/JakeDudum" target="_blank" rel="noopener noreferrer">
+                            <FontAwesomeIcon id="github" icon={faGithubSquare} size="3x" />
+                        </a>
+                    </div>
+                    <div className="linkContainer">
+                        <a href="https://docs.google.com/forms/d/e/1FAIpQLSfvWF8egOrjyWmH-t21TmTB32taW6Y7NSMRj4r79DYWd0-tJw/viewform?usp=sf_link" target="_blank" rel="noopener noreferrer">
+                            <FontAwesomeIcon id="email" icon={faEnvelope} size="3x" />
+                        </a>
+                    </div>
+                </div>
             </div>
         </section>
     )
--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -17,16 +17,15 @@ class Navbar extends Component {
         return (
             <div>
                 <nav className="navbar">
-                    <h1 className="name">Jake Dudum</h1>
                     <div className="links">
-                        <a href="#aboutMe" className="link">About Me</a>
-                        <a href="#project-background" className="link">Projects</a>
+                        <a href="#landing" className="link">About</a>
+                        <a href="#project-title" className="link">Projects</a>
                         <a href="https://docs.google.com/forms/d/e/1FAIpQLSfvWF8egOrjyWmH-t21TmTB32taW6Y7NSMRj4r79DYWd0-tJw/viewform?usp=sf_link" target="_blank" rel="noopener noreferrer" className="link">Contact</a>
                         <a href={Resume} target="_blank" rel="noopener noreferrer" className="link">Resume</a>
                     </div>
                     <img className="menuIcon" src={Menu} alt="menu" onClick={() => this.toggle("dropdownMenu")} />
                     <div id="navbar-menu" style={{ display: `${this.state.dropdownMenu ? "block" : "none"}` }}>
-                        <a href="#aboutMe" className="link" onClick={() => this.toggle("dropdownMenu")}>About Me</a>
+                        <a href="#landing" className="link" onClick={() => this.toggle("dropdownMenu")}>About</a>
                         <a href="#project-background" className="link" onClick={() => this.toggle("dropdownMenu")}>Projects</a>
                         <a href="https://docs.google.com/forms/d/e/1FAIpQLSfvWF8egOrjyWmH-t21TmTB32taW6Y7NSMRj4r79DYWd0-tJw/viewform?usp=sf_link" target="_blank" rel="noopener noreferrer" className="link">Contact</a>
                         <a href={Resume} target="_blank" rel="noopener noreferrer" className="link">Resume</a>
